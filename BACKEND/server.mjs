67703adbf10e052abfdd8c2db644e5e4b8ab3c84
ST10158423 <104215344+ST10158423@users.noreply.github.com>
2024-11-10 22:27:19 +0200

    Update server.mjs

--- BACKEND/server.mjs
+++ BACKEND/server.mjs
@@ -50,7 +50,7 @@ app.use('/employee', employeeRoutes); // Register the employee routes with the p
 // Connect to the database and start the HTTPS server
 connectToDatabase().then(() => {
   const httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(PORT, () => {
+  httpsServer.listen(PORT, '0.0.0.0', () => { // Bind to '0.0.0.0' to allow external access
     console.log(`HTTPS Server is running on https://localhost:${PORT}`);
   });
 }).catch(err => {
